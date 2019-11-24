48b2474025273779833f4c2373f4e53c74b01c07
Irina Kostadinova <irina.draganova@gmail.com>
2019-11-24 04:35:31 +0200

    Areas bug fixed

--- IriOnCocktailService.App/Areas/Magician/Controllers/AboutController.cs
+++ IriOnCocktailService.App/Areas/Magician/Controllers/AboutController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IriOnCocktailService.App.Areas.Magician.Controllers
 {
+    [Area("Magician")]
+    [Authorize(Roles = "CocktailMagician")]
     public class AboutController : Controller
     {
         public IActionResult Index()
